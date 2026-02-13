4f17d2c40d065bfd119430c367885147f1b46671
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-13 13:00:17 +0530

    Create computed.store.js

---
+++ js/store/computed.store.js
@@ -0,0 +1,13 @@
+export const computedStore = {
+
+  skuSales: {},
+  styleSales: {},
+  totalDays: 0,
+
+  clear() {
+    this.skuSales = {};
+    this.styleSales = {};
+    this.totalDays = 0;
+  }
+
+};
